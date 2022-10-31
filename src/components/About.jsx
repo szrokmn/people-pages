9e4cff99a5cc54f880a7a9d671f3e05353887dba
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-31 12:53:58 +0300

    people.jsx finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { FaGlobeAmericas, FaHeadset, FaBookOpen } from 'react-icons/fa';
+import { BsFillArrowDownCircleFill } from "react-icons/bs"
 
 const About = () => {
   return (
-    <div>
-      about
-    </div>
+    <article className='container p-3'>
+
+      <h1 className='text-center'>ABOUT US</h1>
+
+      <div className='row text-center d-flex justify-content-center'>
+      <i className="fas fa-globe-americas fa-2x"></i>
+      <h4>Online Courses</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus magni distinctio, dolorum labore quasi et nobis animi sit blanditiis explicabo possimus perferendis veniam nam consequuntur cupiditate similique ullam maxime?</p>
+      </div>
+
+      <div>
+       <i className="fas fas fa-headset fa-2x"></i>
+      <h4>Full-time Mentoring</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus magni distinctio, dolorum labore quasi et nobis animi sit blanditiis explicabo possimus perferendis veniam nam consequuntur cupiditate similique ullam maxime?</p>
+      </div>
+
+      <div>
+      <i className="BsFillArrowDownCircleFill"></i>
+      <h4>Learn Yourself</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatibus magni distinctio, dolorum labore quasi et nobis animi sit blanditiis explicabo possimus perferendis veniam nam consequuntur cupiditate similique ullam maxime?</p>
+      </div>
+
+    </article>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import About from '../components/About'
 
 const Home = () => {
   return (
     <div>
-      
+      <About/>
     </div>
   )
 }
